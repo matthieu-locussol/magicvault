f3344277538a7de5bd2bb36448c11cfe28819770
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-21 23:05:30 +0100

    feat: added SearchBar to Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Header from '@/components/Header/Header';
+import SearchBar from '@/components/Header/SearchBar';
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -13,12 +14,14 @@ const useStyles = makeStyles((theme: Theme) =>
 			justifyContent: 'space-between',
 			width: '100%',
 			maxWidth: theme.breakpoints.values.lg,
-			paddingLeft: theme.spacing(2),
-			paddingRight: theme.spacing(2),
-			marginTop: theme.spacing(14),
+			paddingLeft: theme.spacing(3),
+			paddingRight: theme.spacing(3),
+			marginTop: theme.spacing(26),
 			[theme.breakpoints.down('sm')]: {
 				paddingLeft: 0,
 				paddingRight: 0,
+				marginTop: theme.spacing(24),
+				flexDirection: 'column',
 			},
 		},
 	}),
@@ -26,15 +29,17 @@ const useStyles = makeStyles((theme: Theme) =>
 
 interface LayoutProps {
 	hideHeader?: boolean;
+	hideSearch?: boolean;
 	children: React.ReactNode;
 }
 
-const Layout = ({ hideHeader, children }: LayoutProps) => {
+const Layout = ({ hideHeader, hideSearch, children }: LayoutProps) => {
 	const classes = useStyles();
 
 	return (
 		<div className={classes.root}>
 			{!hideHeader && <Header />}
+			{!hideSearch && <SearchBar />}
 			<div className={classes.layout}>{children}</div>
 		</div>
 	);
