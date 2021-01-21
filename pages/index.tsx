3aafb45bf9245ac8c73b2b7f116dd565a1e0f4ae
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-21 18:31:03 +0100

    feat: updated index page to display two columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+import Card from '@/components/Card';
+import Column from '@/components/Layout/Column';
+import Layout from '@/components/Layout/Layout';
 
 const Home = () => {
 	return (
-		<div>
-			Hello world! <Button>lolipop</Button>
-		</div>
+		<Layout>
+			<Column type="left">
+				<Card>
+					<Typography variant="h4" gutterBottom>
+						Hello world!
+					</Typography>
+					<Button variant="contained" color="primary" size="large">
+						My special button
+					</Button>
+				</Card>
+			</Column>
+			<Column type="right">
+				<Card>
+					<Typography variant="h4" gutterBottom>
+						Hello world!
+					</Typography>
+					<Button variant="contained" color="primary" size="large">
+						My special button
+					</Button>
+				</Card>
+			</Column>
+		</Layout>
 	);
 };
 
