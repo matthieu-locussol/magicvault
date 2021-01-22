0c80e79473b3e6275c01bc3de2e001013183dcbd
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-22 10:04:39 +0100

    feat: added a Logo component

---
+++ components/Header/Logo.tsx
@@ -0,0 +1,30 @@
+import NextLink from 'next/link';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Link from '@/components/Link';
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			fontFamily: 'Metamorphous',
+			marginRight: 'auto',
+			'&:hover': {
+				color: `${theme.palette.text.primary}CC`,
+				textDecoration: 'none',
+			},
+		},
+	}),
+);
+
+const Logo = () => {
+	const classes = useStyles();
+
+	return (
+		<NextLink href="/" passHref>
+			<Link color="textPrimary" variant="h5" className={classes.root}>
+				<b>M</b>agic<b>V</b>ault
+			</Link>
+		</NextLink>
+	);
+};
+
+export default Logo;
