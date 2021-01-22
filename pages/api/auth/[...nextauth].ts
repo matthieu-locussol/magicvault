f90e7c2eee0526d22be008a83324970c73ff106c
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-22 17:41:12 +0100

    feat: added NextAuth authentication with Google

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth, { InitOptions } from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options: InitOptions = {
+	providers: [
+		Providers.Google({
+			clientId: process.env.GOOGLE_ID || '',
+			clientSecret: process.env.GOOGLE_SECRET || '',
+		}),
+	],
+	secret: process.env.SECRET,
+};
+
+export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options);
