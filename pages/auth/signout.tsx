c7be67c21f8b784164a1a686cb743863d15108de
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-23 14:21:31 +0100

    styles: title is now centered

--- pages/auth/signout.tsx
+++ pages/auth/signout.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Card from '@/components/Card';
 import Column from '@/components/Layout/Column';
 import Layout from '@/components/Layout/Layout';
 
-const SignOut = () => (
-	<Layout>
-		<Column type="full">
-			<Card>
-				<Typography variant="h5" gutterBottom>
-					You successfully logged out!
-				</Typography>
-			</Card>
-		</Column>
-	</Layout>
+const useStyles = makeStyles(() =>
+	createStyles({
+		title: {
+			textAlign: 'center',
+		},
+	}),
 );
+
+const SignOut = () => {
+	const classes = useStyles();
+
+	return (
+		<Layout>
+			<Column type="full">
+				<Card>
+					<Typography variant="h5" className={classes.title}>
+						You successfully logged out!
+					</Typography>
+				</Card>
+			</Column>
+		</Layout>
+	);
+};
 
 export default SignOut;
