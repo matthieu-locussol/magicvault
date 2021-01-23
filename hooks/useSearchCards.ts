37f79352cfaef908e04606235813b0471d50caf7
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-23 14:21:12 +0100

    fix: don't search again while searching

--- hooks/useSearchCards.ts
+++ hooks/useSearchCards.ts
@@ -2,16 +2,21 @@ import { useStore } from '@/store';
 import { searchCards } from '@/api/scryfall';
 
 export const useSearchCards = () => {
-	const [, dispatch] = useStore();
+	const [store, dispatch] = useStore();
 
 	return async (query: string) => {
-		try {
-			dispatch({ type: 'START_SEARCH' });
-			const results = await searchCards(query);
-			dispatch({ type: 'END_SEARCH', payload: results.data.data });
-		} catch (error) {
-			console.error(error);
-			dispatch({ type: 'END_SEARCH' });
+		if (!store.search.loading) {
+			try {
+				dispatch({ type: 'START_SEARCH', terms: query });
+				const results = await searchCards(query);
+				if (results.status === 200) {
+					dispatch({ type: 'END_SEARCH', results: results.data.data });
+				} else {
+					dispatch({ type: 'END_SEARCH', results: [] });
+				}
+			} catch (error) {
+				dispatch({ type: 'END_SEARCH', results: undefined });
+			}
 		}
 	};
 };
