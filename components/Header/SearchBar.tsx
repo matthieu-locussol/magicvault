a4345bb5506d368453e431f49bc78bcbedc9d033
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-23 12:18:02 +0100

    feat: uses Scryfall API to search cardss

--- components/Header/SearchBar.tsx
+++ components/Header/SearchBar.tsx
@@ -1,6 +1,8 @@
+import { FormEvent, useState } from 'react';
 import { AppBar, AppBarProps, Toolbar, TextField, InputAdornment, Button } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { Search as SearchIcon } from '@material-ui/icons';
+import { searchCards } from '@/api/scryfall';
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -20,6 +22,9 @@ const useStyles = makeStyles((theme: Theme) =>
 				marginTop: theme.spacing(9),
 			},
 		},
+		form: {
+			width: '100%',
+		},
 		toolbar: {
 			width: '100%',
 			display: 'flex',
@@ -37,23 +42,49 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const SearchBar = (props: AppBarProps) => {
 	const classes = useStyles();
+	const [loading, setLoading] = useState(false);
+	const [searchValue, setSearchValue] = useState('');
+
+	const search = async (e?: FormEvent<HTMLFormElement>) => {
+		e?.preventDefault();
+
+		setLoading(true);
+
+		try {
+			const results = await searchCards(searchValue);
+			console.log(results);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
 
 	return (
 		<AppBar className={classes.root} {...props}>
 			<Toolbar className={classes.toolbar}>
-				<TextField
-					variant="outlined"
-					className={classes.searchbar}
-					InputProps={{
-						endAdornment: (
-							<InputAdornment position="end">
-								<Button variant="contained" color="primary" className={classes.button}>
-									<SearchIcon />
-								</Button>
-							</InputAdornment>
-						),
-					}}
-				/>
+				<form onSubmit={search} className={classes.form}>
+					<TextField
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+						variant="outlined"
+						className={classes.searchbar}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<Button
+										disabled={loading}
+										variant="contained"
+										color="primary"
+										className={classes.button}
+										onClick={() => search()}>
+										<SearchIcon />
+									</Button>
+								</InputAdornment>
+							),
+						}}
+					/>
+				</form>
 			</Toolbar>
 		</AppBar>
 	);
