4775ad5ca899867ad2b2121d816b979b3a013779
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-25 13:36:24 +0100

    feat: added a CardResults component and uses it in Homepage

---
+++ components/Card/Results/CardResults.tsx
@@ -0,0 +1,106 @@
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import {
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TablePagination,
+	TableRow,
+	LinearProgress,
+} from '@material-ui/core';
+import ManaCost from '@/components/Card/ManaCost';
+import { useSearchCards } from '@/hooks/useSearchCards';
+import { useStore } from '@/store';
+import { Card } from '@/types/Card';
+
+const useStyles = makeStyles(() =>
+	createStyles({
+		root: {
+			width: '100%',
+		},
+		row: {
+			'&:nth-of-type(odd)': {
+				backgroundColor: '#ffffff',
+			},
+			'&:nth-of-type(even)': {
+				backgroundColor: '#f4f8fc',
+			},
+		},
+	}),
+);
+
+const CardResults = () => {
+	const classes = useStyles();
+	const [store] = useStore();
+	const searchCards = useSearchCards();
+
+	const handleChangePage = (_: unknown, newPage: number) => {
+		searchCards(store.search.query, newPage);
+	};
+
+	const columns = {
+		SET: (card: Card) => card.set.toUpperCase(),
+		'N°': (card: Card) => card.collector_number,
+		NAME: (card: Card) => card.name,
+		COST: (card: Card) => <ManaCost value={card.mana_cost} />,
+	};
+
+	return (
+		<Paper className={classes.root}>
+			<TablePagination
+				rowsPerPageOptions={[175]}
+				component="div"
+				count={store.search.resultsCount}
+				rowsPerPage={175}
+				page={store.search.page}
+				onChangePage={handleChangePage}
+				labelDisplayedRows={({ from, to, count }) =>
+					store.search.loading ? '' : `${from}-${to} of ${count !== -1 ? count : `more than ${to}`}`
+				}
+			/>
+			{store.search.results && store.search.results.length > 0 ? (
+				<TableContainer>
+					<Table stickyHeader size="small" aria-label="sticky table">
+						<TableHead>
+							<TableRow>
+								{Object.keys(columns).map((column) => (
+									<TableCell key={column}>
+										<b>{column}</b>
+									</TableCell>
+								))}
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{store.search.results.map((card: any, idx: number) => (
+								<TableRow hover className={classes.row} key={idx}>
+									{Object.values(columns).map((getter, idc) => (
+										<TableCell key={`${idx}-${idc}`}>{getter(card)}</TableCell>
+									))}
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			) : store.search.loading ? (
+				<LinearProgress />
+			) : (
+				'Start searching please.'
+			)}
+			<TablePagination
+				rowsPerPageOptions={[175]}
+				component="div"
+				count={store.search.resultsCount}
+				rowsPerPage={175}
+				page={store.search.page}
+				onChangePage={handleChangePage}
+				labelDisplayedRows={({ from, to, count }) =>
+					store.search.loading ? '' : `${from}-${to} of ${count !== -1 ? count : `more than ${to}`}`
+				}
+			/>
+		</Paper>
+	);
+};
+
+export default CardResults;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,17 @@
 import React from 'react';
-import { Button, Typography } from '@material-ui/core';
 import Card from '@/components/Card';
 import Column from '@/components/Layout/Column';
 import Layout from '@/components/Layout/Layout';
-import { useStore } from '@/store';
-import CardImage from '@/components/Card/CardImage';
-import ManaCost from '@/components/Card/ManaCost';
+import CardResults from '@/components/Card/Results/CardResults';
 
-const Home = () => {
-	const [store] = useStore();
-
-	return (
-		<Layout>
-			<Column type="left">
-				<Card>
-					<Typography variant="h4" gutterBottom>
-						Hello world!
-					</Typography>
-					<Button variant="contained" color="primary" size="large">
-						My special button
-					</Button>
-				</Card>
-			</Column>
-			<Column type="right">
-				<Card>
-					<Typography variant="h4" gutterBottom>
-						Results
-					</Typography>
-					{store.search.loading
-						? '...'
-						: store.search.results === undefined
-						? `No results for "${store.search.terms}".`
-						: store.search.results.length === 0
-						? 'You can search using the SearchBar.'
-						: store.search.results.map((res, idx) => (
-								<>
-									<Typography variant="body2" key={idx}>
-										{res.name}
-									</Typography>
-									<ManaCost value={res.mana_cost} />
-									{console.log(res)}
-									<CardImage data={res} />
-								</>
-						  ))}
-				</Card>
-			</Column>
-		</Layout>
-	);
-};
+const Home = () => (
+	<Layout>
+		<Column type="full">
+			<Card noPadding>
+				<CardResults />
+			</Card>
+		</Column>
+	</Layout>
+);
 
 export default Home;
