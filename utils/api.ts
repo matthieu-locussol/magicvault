7d4fdf39b292cb5aae7df6effca6c3dfdae1dd55
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-27 13:49:55 +0100

    feat: added new API calls functions

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,49 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export const api = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
 });
 
-export const getOwnedIds = async (email: string) =>
+export type CardIdentifier = {
+	id: string;
+	amount: number;
+};
+
+type OwnedIdentifiersResponse = {
+	statusCode: number;
+	data: CardIdentifier[];
+};
+
+export const getOwnedIdentifiers = async (email: string): Promise<AxiosResponse<OwnedIdentifiersResponse>> =>
 	api.post('', {
-		type: 'ownedCards',
+		type: 'ownedIdentifiers',
+		email,
+	});
+
+type UpdateOwnedIdentifiersResponse = {
+	statusCode: number;
+	data: string;
+};
+
+export const updateOwnedIdentifiers = async (
+	email: string,
+	identifiers: CardIdentifier[],
+): Promise<AxiosResponse<UpdateOwnedIdentifiersResponse>> =>
+	api.post('', {
+		type: 'updateOwnedIdentifiers',
+		email,
+		identifiers,
+	});
+
+type CreateOwnedIdentifiersProps = {
+	statusCode: number;
+	data: string;
+};
+
+export const createOwnedIdentifiers = async (
+	email: string,
+): Promise<AxiosResponse<CreateOwnedIdentifiersProps>> =>
+	api.post('', {
+		type: 'createOwnedIdentifiers',
 		email,
 	});
