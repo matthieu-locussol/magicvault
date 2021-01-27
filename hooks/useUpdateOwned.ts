91710bdc3f9f7db6b5702d672ec0cda5133a5c09
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-27 13:52:11 +0100

    feat: added a useUpdateOwned hook

---
+++ hooks/useUpdateOwned.ts
@@ -0,0 +1,20 @@
+import { useStore } from '@/store';
+import { useSession } from 'next-auth/client';
+import { Card } from '@/types/Card';
+import { updateOwnedIdentifiers } from '@/utils/api';
+
+export const useUpdateOwned = () => {
+	const [session] = useSession();
+	const [store, dispatch] = useStore();
+
+	return async (card: Card, amount: number) => {
+		if (session) {
+			dispatch({ type: 'START_UPDATE_OWNED', card, amount });
+
+			if (session.user.email) {
+				await updateOwnedIdentifiers(session.user.email, store.profile.ownedIdentifiers);
+				dispatch({ type: 'END_UPDATE_OWNED' });
+			}
+		}
+	};
+};
