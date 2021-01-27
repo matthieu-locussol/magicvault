95ff5bccbcc0cdf4bbf44d41d49542d7abb6f6a2
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-27 14:41:34 +0100

    feat: if no users_owned entries is associated to the current user in FaunaDB, it now gets created automatically

--- hooks/useUpdateProfile.ts
+++ hooks/useUpdateProfile.ts
@@ -1,8 +1,10 @@
 import { useStore } from '@/store';
-import { getOwnedIds } from '@/utils/api';
+import { getOwnedIdentifiers, createOwnedIdentifiers } from '@/utils/api';
 import { getCards } from '@/utils/scryfall';
 import { useSession } from 'next-auth/client';
 import { Card } from '@/types/Card';
+
+const CARDS_PER_PAGE = 75;
 
 export const useUpdateProfile = () => {
 	const [session] = useSession();
@@ -11,19 +13,31 @@ export const useUpdateProfile = () => {
 	return async () => {
 		if (session) {
 			dispatch({ type: 'START_UPDATE_PROFILE' });
-			const responseOwnedsIds = await getOwnedIds(session?.user.email || '');
-			const ownedIds = responseOwnedsIds.data.body[0].data.cards.map((id: string) => ({ id }));
 
-			const callsCount = Math.ceil(ownedIds.length / 75);
-			let ownedCards: Card[] = [];
+			if (session.user.email) {
+				let responseOwnedsIdentifiers = await getOwnedIdentifiers(session.user.email);
+				let ownedIdentifiers = responseOwnedsIdentifiers.data.data;
 
-			for (let i = 0; i < callsCount; ++i) {
-				const ids = ownedIds.slice(i * 75, (i + 1) * 75);
-				const responseOwnedCards = await getCards(ids);
-				ownedCards = [...ownedCards, ...responseOwnedCards.data.data];
+				if (!(ownedIdentifiers?.length > 0)) {
+					await createOwnedIdentifiers(session.user.email);
+
+					responseOwnedsIdentifiers = await getOwnedIdentifiers(session.user.email);
+					ownedIdentifiers = responseOwnedsIdentifiers.data.data;
+				}
+
+				const cardsIds = ownedIdentifiers.map((identifier) => ({ id: identifier.id }));
+
+				const callsCount = Math.ceil(cardsIds.length / CARDS_PER_PAGE);
+				let ownedCards: Card[] = [];
+
+				for (let i = 0; i < callsCount; ++i) {
+					const ids = cardsIds.slice(i * CARDS_PER_PAGE, (i + 1) * CARDS_PER_PAGE);
+					const responseOwnedCards = await getCards(ids);
+					ownedCards = [...ownedCards, ...responseOwnedCards.data.data];
+				}
+
+				dispatch({ type: 'END_UPDATE_PROFILE', ownedCards, ownedIdentifiers });
 			}
-
-			dispatch({ type: 'END_UPDATE_PROFILE', ownedCards });
 		}
 	};
 };
