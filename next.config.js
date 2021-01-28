6c78883eb78d1f351194a2db566694035ab79816
Matthieu Locussol <matthieu@galadrim.fr>
2021-01-28 10:18:03 +0100

    feat: / redirects to /search

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
 	images: {
 		domains: ['c1.scryfall.com'],
 	},
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/search',
+				permanent: true,
+			},
+		];
+	},
 };
